a0be1d177cc659dd2e118dcefa5335551493b22a
DinulDassanayake <57146576+DinulDassanayake@users.noreply.github.com>
2020-10-01 20:33:21 +0530

    Update 7 - Jumping on the Clouds.py

--- 1 - Algorithms/1 - Implementation/7 - Jumping on the Clouds.py
+++ 1 - Algorithms/1 - Implementation/7 - Jumping on the Clouds.py
@@ -1,13 +1,12 @@
 n = int(input().strip())
 clouds = [int(tmp) for tmp in input().strip().split(' ')]
-current = 0
-end = n - 1
-jumps = 0
-while current < end:
-	if ((current + 2) <= end) and (clouds[current + 2] == 0):
-		current += 2
-		jumps += 1
-	elif clouds[current + 1] == 0:
-		current += 1
-		jumps += 1
-print(jumps)
+ans = 0
+i = 0
+while i < n - 1:
+    if i + 2 >= n or c[i + 2] == 1:   # Not possible to make a jump of size 2
+        i = i + 1
+        ans = ans + 1
+    else:
+        i = i + 2
+        ans = ans + 1
+return ans
